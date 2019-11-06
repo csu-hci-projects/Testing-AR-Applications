5158c39bed8965c80df0d93a462d7831d3e515e0
Richard LaFranchi <richard.lafranchi@charter.com>
2019-11-06 14:29:19 -0700

    add building model

---
+++ CS567-Project/Building.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  CS567-Project
+//
+//  Created by Lafranchi, Richard A on 11/6/19.
+//  Copyright © 2019 Richard LaFranchi. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+
+class Building {
+    var corners: [simd_float2];
+    init() {
+        corners = [];
+    }
+    
+    func addCorner(corner: simd_float2) {
+        corners.append(corner);
+    }
+    
+    func draw(scene: SCNScene) {
+        
+    }
+}
